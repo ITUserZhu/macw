d239c1caad3586861280e6eb6f28312bd77e9fc0
zhu <17601399112@163.com>
2019-12-30 10:57:30 +0800

    添加word模板

---
+++ pc/es6/word-content.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Liliang Zhu 
+ * @Date: 2019-12-27 16:55:25 
+ * @Last Modified by: Liliang Zhu
+ * @Last Modified time: 2019-12-27 16:56:08
+ * word模板内容
+ */
+
+// 内容公共板块
+import './modules/content-operate';

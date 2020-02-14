c82c80dbeeb6ff15a887f69f20184d6c9a5670fa
MrZhu <17601399112@163.com>
2020-02-14 13:51:58 +0800

    修改页面

---
+++ pc/es6/necessary-install.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: Liliang Zhu 
+ * @Date: 2020-02-13 12:45:48 
+ * @Last Modified by: Liliang Zhu
+ * @Last Modified time: 2020-02-13 12:55:51
+ * 首页
+ */
+
+// 引入公用模块
+import './components/common';
+// 壁纸鼠标效果
+import './plugins/wallpaper';
+// 引入视频播放功能
+import VideoPlay from './components/video-play';
+
+$(function() {
+  // 壁纸鼠标效果
+  $('ul.wallpaper').on('mouseenter', 'li', function () {
+    $(this).hoverCheck();
+  }).on('mouseleave', 'li', function () {
+    $(this).leaveCheck();
+  })
+
+   // 视频鼠标功能
+   const $videos = $('.necessary').find('ul.video');
+
+   new VideoPlay($videos, {
+     item: 'li',
+     fade: true,
+     oftX: 5,
+     conCallback(src) {
+       return `<video autoplay muted loop><source src="${ src }" type="video/mp4">您的浏览器不支持html5播放器</video>`;
+     }
+   })
+});
---
+++ pc/es6/topic-index.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: Liliang Zhu 
+ * @Date: 2020-02-12 10:53:57 
+ * @Last Modified by: Liliang Zhu
+ * @Last Modified time: 2020-02-12 11:35:42
+ * 专题首页
+ */
+
+// 引入公用模块
+import './components/common';
+// 引入工具
+
+import {
+  toggleActive
+} from './util';
+
+$(function () {
+  const $topicClassify = $('.topic-classify'),
+    $nav = $topicClassify.find('.classify-nav').children('a'),
+    $lis = $topicClassify.find('ul').children('li');
+  $nav.on('mouseenter', function (e) {
+    var _index = $(this).index();
+    toggleActive([$(this), $lis.eq(_index)]);
+  });
+});

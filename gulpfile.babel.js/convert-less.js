5db4a7476dc96fc0554384e9344c353d0210f094
zhu <17601399112@163.com>
2020-03-05 16:08:14 +0800

    测试

--- gulpfile.babel.js/convert-less.js
+++ gulpfile.babel.js/convert-less.js
@@ -1,57 +1,75 @@
 /*
- * @Author: Liliang Zhu 
- * @Date: 2020-01-18 18:18:52 
+ * @Author: Liliang Zhu
+ * @Date: 2020-01-18 18:18:52
  * @Last Modified by: Liliang Zhu
  * @Last Modified time: 2020-02-26 14:50:57
  * 编译less
  */
 
 // gulp模块
-import {
-  src,
-  dest,
-  lastRun
-} from 'gulp';
+import { src, dest, lastRun } from "gulp";
 
 // less语法转译
-import less from 'gulp-less';
+import less from "gulp-less";
 // css添加前缀
-import lessAutoperfix from 'less-plugin-autoprefix';
+import lessAutoperfix from "less-plugin-autoprefix";
 // 压缩css
-import mixCss from 'gulp-clean-css';
+import mixCss from "gulp-clean-css";
 // 仅编译改变的文件
-import changed from 'gulp-changed';
+import changed from "gulp-changed";
 // 重命名
-import rename from 'gulp-rename';
+import rename from "gulp-rename";
 // 生成版本号
-import rev from 'gulp-rev';
+import rev from "gulp-rev";
+// 条件判断
+import gulpif from "gulp-if";
+// 移动端自动px转换rem
+import postcss from "gulp-postcss";
+import pxtorem from "postcss-pxtorem";
 // 本地服务同步刷新
-import browser from 'browser-sync';
+import browser from "browser-sync";
 const browserSync = browser.create();
 
 // css编译前缀
 const autoprefix = new lessAutoperfix();
 
-let convertLess = (file, dist) => {
+let convertLess = (file, dist, type) => {
   return src(`${file}less/*.less`, {
-      since: lastRun(convertLess, 100)
-    })
-    .pipe(less({
-      plugins: [autoprefix]
-      // 生成前缀
-    }))
-    .pipe(mixCss({
-      keepSpecialComments: '*'
-      //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
-    }))
-    .pipe(rename(path => path.basename += '.min'))
+    since: lastRun(convertLess, 100)
+  })
+    .pipe(
+      less({
+        plugins: [autoprefix]
+        // 生成前缀
+      })
+    )
+    .pipe(
+      gulpif(
+        type === "m",
+        postcss(
+          pxtorem({
+            rootValue: 100,
+            propList: ["*"]
+          })
+        )
+      )
+    )
+    .pipe(
+      mixCss({
+        keepSpecialComments: "*"
+        //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+      })
+    )
+    .pipe(rename(path => (path.basename += ".min")))
     .pipe(rev())
     .pipe(dest(`${dist}css`))
     .pipe(rev.manifest())
     .pipe(dest(`${file}mapjson/css`))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 };
 
 export default convertLess;
--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,29 +1,23 @@
-import {
-  src,
-  dest,
-  series,
-  parallel,
-  watch,
-} from 'gulp';
+import { src, dest, series, parallel, watch } from "gulp";
 
 // 本地服务同步刷新
-import browser from 'browser-sync';
+import browser from "browser-sync";
 const browserSync = browser.create();
 // 引入功能组件
-import del from 'del';
-import convertLess from './convert-less';
-import convertJs from './convert-js';
-import convertHtml from './convert-html';
-import copyFile from './static-copy';
+import del from "del";
+import convertLess from "./convert-less";
+import convertJs from "./convert-js";
+import convertHtml from "./convert-html";
+import copyFile from "./static-copy";
 
 // 域名与站点名称
 const host = {
-  hostName: 'macw',
-  hostTitle: 'Macw网'
+  hostName: "macw",
+  hostTitle: "Macw网"
 };
 
 // 开发项目类型
-const devType = 'pc';
+const devType = "pc";
 
 // 本地目录
 const filePath = `project/${devType}/`;
@@ -31,34 +25,27 @@ const filePath = `project/${devType}/`;
 const distResourcesPath = `xs_cms/pub_${devType}/assets/`;
 const distPagesPath = `xs_cms/app/${devType}/view/`;
 // 资源路径
-const baseProjectPath = '/assets/';
+const baseProjectPath = "/assets/";
 
 // 打印提示颜色
-const word_color = '\x1b[91m'; // 橘黄
+const word_color = "\x1b[91m"; // 橘黄
 
 // 删除css文件
 export const delCssFile = () => {
   console.log(word_color, `${new Date()} 清除css`);
-  return del([
-    `${distResourcesPath}css`
-  ])
+  return del([`${distResourcesPath}css`]);
 };
 
 // 删除js文件
 export const delJsFile = () => {
   console.log(word_color, `${new Date()} 清除js`);
-  return del([
-    `${distResourcesPath}js`
-  ])
+  return del([`${distResourcesPath}js`]);
 };
 
 // 删除资源文件夹
 export const delStaticFile = () => {
   console.log(word_color, `${new Date()} 清除资源`);
-  return del([
-    `${distResourcesPath}images`,
-    `${distResourcesPath}fonts`,
-  ])
+  return del([`${distResourcesPath}images`, `${distResourcesPath}fonts`]);
 };
 // 导出任务
 // 复制文件
@@ -71,14 +58,15 @@ export const copyStatic = cb => {
 // 复制js静态文件
 export const copyJs = () => {
   console.log(word_color, `${new Date()} copy静态js`);
-  return src(`${filePath}es6/static/**`)
-    .pipe(dest(`${distResourcesPath}js/static`));
+  return src(`${filePath}es6/static/**`).pipe(
+    dest(`${distResourcesPath}js/static`)
+  );
 };
 
 // 编译css
 export const compileCss = series(delCssFile, cb => {
   console.log(word_color, `${new Date()} 编译css`);
-  convertLess(filePath, distResourcesPath);
+  convertLess(filePath, distResourcesPath, devType);
   cb();
 });
 // 编译js
@@ -95,27 +83,47 @@ export const freshHtml = cb => {
   cb();
 };
 
-
 // 监测文件变化
 let watchFiles = () => {
   browserSync.init({});
 
-  watch(`${filePath}less/**/*.less`, {
-    delay: 500,
-  }, compileCss);
+  watch(
+    `${filePath}less/**/*.less`,
+    {
+      delay: 500
+    },
+    compileCss
+  );
 
-  watch(`${filePath}es6/**/*.js`, {
-    delay: 500,
-  }, series(compileJs, copyJs));
+  watch(
+    `${filePath}es6/**/*.js`,
+    {
+      delay: 500
+    },
+    series(compileJs, copyJs)
+  );
 
-  watch(`${filePath}pages/**`, {
-    delay: 500,
-  }, freshHtml);
+  watch(
+    `${filePath}pages/**`,
+    {
+      delay: 500
+    },
+    freshHtml
+  );
 
-  watch(`${filePath}mapjson/**/*.json`, {
-    delay: 500,
-  }, freshHtml);
+  watch(
+    `${filePath}mapjson/**/*.json`,
+    {
+      delay: 500
+    },
+    freshHtml
+  );
 };
 
 // 默认任务
-exports.default = series(parallel(compileCss, series(compileJs, copyJs)), series(delStaticFile ,copyStatic), freshHtml, watchFiles);
+exports.default = series(
+  parallel(compileCss, series(compileJs, copyJs)),
+  freshHtml,
+  series(delStaticFile, copyStatic),
+  watchFiles
+);

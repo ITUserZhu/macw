c8a2637ea97cf080ee25f9f7cf8bd5426441ee65
zhu <17601399112@163.com>
2020-03-24 11:24:56 +0800

    添加pc 壁纸排行榜

--- pc/es6/rank.js
+++ pc/es6/rank.js
@@ -1,22 +1,31 @@
 /*
- * @Author: Liliang Zhu 
- * @Date: 2019-11-19 14:53:57 
+ * @Author: Liliang Zhu
+ * @Date: 2019-11-19 14:53:57
  * @Last Modified by: Liliang Zhu
  * @Last Modified time: 2019-11-21 11:35:42
  * 排行榜
  */
 // 引入公用模块
-import './components/common';
+import "./components/common";
+import "./plugins/wallpaper";
 // 引入工具
-import {
-  toggleActive
-} from './util';
+import { toggleActive } from "./util";
 
-$(function () {
-  const $pagi = $('.rank-pagi'),
-    $rankUls = $('section').children('ul');
-  $pagi.on('click', 'span', function (e) {
-    var _index = $(this).index();
+$(() => {
+  const $pagi = $(".rank-pagi"),
+    $rankUls = $("section").children("ul");
+  $pagi.on("click", "span", function(e) {
+    let _index = $(this).index();
     toggleActive([$(this), $rankUls.eq(_index)]);
   });
+
+  if ($(".rank-wallpaper").length) {
+    $("ul.wallpaper")
+      .on("mouseenter", "li", function() {
+        $(this).hoverCheck();
+      })
+      .on("mouseleave", "li", function() {
+        $(this).leaveCheck();
+      });
+  }
 });

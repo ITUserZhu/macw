f4af0aa9789f86927e9b2feec63499531af8853c
zhu <17601399112@163.com>
2020-03-26 18:20:09 +0800

    添加移动模板

---
+++ mobile/es6/material-content.js
@@ -0,0 +1,19 @@
+import "./common/index";
+import "./plugins/waterfall";
+
+$(() => {
+  // 查询设备尺寸定义瀑布流宽度与间距
+  const colW = (window.outerWidth - window.outerWidth * 0.1) * 0.485;
+  const colM = (window.outerWidth - window.outerWidth * 0.1) * 0.03;
+
+  $(window).on("load", () => {
+    $(".material-waterfall_list").waterfall({
+      itemCls: "item",
+      minCol: 2,
+      colWidth: colW,
+      gutterWidth: colM,
+      gutterHeight: colM,
+      loadingMsg: ""
+    });
+  });
+});
--- mobile/es6/video-index.js
+++ mobile/es6/video-index.js
@@ -4,9 +4,6 @@ import Swiper from "swiper";
 $(() => {
   // 首页头部轮播
   new Swiper(".swiper-container", {
-    initialSlide: 1,
-    slidesPerView: 2,
-    slidesOffsetBefore: 87.5,
     loop: true,
     pagination: {
       el: ".swiper-pagination"
